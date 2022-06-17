3c27a12f8a4f2791fd6bfaf6791f74cf03e36bbf
Tristan247 <56597679+Tristan247@users.noreply.github.com>
2022-06-17 10:13:10 +0200

    update again 2

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -3,6 +3,11 @@ import multer from "multer";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import { receiveMessageOnPort } from "worker_threads";
+import * as Storage from "@google-cloud/storage";
+import fs from 'fs'
+import {Base64} from 'js-base64';
+import FormData from "form-data";
+import axios from "axios";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
